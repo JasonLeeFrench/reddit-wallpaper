b264e39e7b767bef227fe1ee5dfb3c8fe5d86901
frenchj <jason.french@wsj.com>
2016-12-25 21:43:43 +0000

    remove superfluous lines

--- wallpaper/__main__.py
+++ wallpaper/__main__.py
@@ -15,15 +15,12 @@ def url_to_file(url, name):
   f.close()
 
 def main():
-
   if os.getuid() != 0:
     sys.exit("i need to run as sudo 😐")
-
   if len(sys.argv) >= 2:
     query = sys.argv[1]
   else:
     query = 'natureporn'
-
   desktop_location = "/Library/Desktop Pictures/"
   purge(desktop_location, "reddit")
   image_name = desktop_location + "reddit" + str(random.random())[2:] + '.jpg'
